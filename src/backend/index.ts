b2a1b3af7ddc3d0a82eefc355d26c9920a7bab06
jonathanmedhed <jonathanmedhed@gmail.com>
2019-06-13 15:16:02 +0100

    x

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,12 +1,11 @@
 import { createApp } from "./config/app";
 
-
 (async () => {
 
     const app = await createApp();
-    const port = process.env.PORT || 8080;
+
     // Start the server
-    app.listen(port, () => {
+    app.listen(8080, () => {
         console.log(
             "The server is running in port 8080!"
         );
